02e3c9ea72a6aab1594c2371b3e4a0ce73014696
Logan <59356357+LoganLakosky@users.noreply.github.com>
2023-08-27 14:31:35 -0500

    Added ability to update a new notes name

--- practice/src/components/mainContentCenterComponents/NotesFile/Notes.tsx
+++ practice/src/components/mainContentCenterComponents/NotesFile/Notes.tsx
@@ -1,12 +1,26 @@
 import "./notes.css";
+import { ChangeEvent, useState } from "react";
 
 export default function Notes() {
+  const [newNoteName, setNewNoteName] = useState<string>("");
+
+  function updateNewNoteName(e: ChangeEvent<HTMLInputElement>) {
+    setNewNoteName(e.target.value);
+    console.log(newNoteName)
+  }
+
   return (
     <div className="notesMainContainer">
       <div className="notesMainTop">
         <div className="newNoteNameContainer">
           <label htmlFor="new-note-input">Enter note name:</label>
-          <input type="text" id="new-note-input" name="new-note-input" />
+          <input
+            value={newNoteName}
+            type="text"
+            id="new-note-input"
+            name="new-note-input"
+            onChange={updateNewNoteName}
+          />
         </div>
         <div className="createNoteBtnContainer">
           <button className="createNoteBtn">Create Note</button>
